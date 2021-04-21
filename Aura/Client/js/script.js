db5fe0db6f687f89681628ba4932e329f658659a
Rinelsia <rinelsia@mail.ru>
2021-04-21 14:55:06 +0300

    Add files via upload

--- Aura/Client/js/script.js
+++ Aura/Client/js/script.js
@@ -1,1 +1,11 @@
 console.log('Файл скрипта подключен и выполнен');
+function drag(ev){
+    console.log(ev);
+}
+function start(ev){
+    console.log(ev);
+}
+//
+function end(ev){
+    console.log(ev);
+}
